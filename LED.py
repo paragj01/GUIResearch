0902205809650e8402a7fa6057cd74e61d112cb1
paragj01 <paragjibhakate01@gmail.com>
2022-02-08 17:33:13 +0530

    LED Correction

--- LED.py
+++ LED.py
@@ -1,45 +1,35 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QDialog
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.uic import loadUi
+from matplotlib.widgets import Widget
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-LED = 8
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+LED = 26
 GPIO.setup(LED, GPIO.OUT)
 
-class App(QWidget):
+class Win(QDialog):
 
     def __init__(self):
-        super().__init__()
-        self.title = 'LED'
-        self.left = 100
-        self.top = 100
-        self.width = 320
-        self.height = 200
-        self.initUI()
+        super(Win,self).__init__()
+        loadUi('dialog.ui',self)
+        self.setWindowTitle('LED')
+        self.btn.clicked.connect(self.on_off)
     
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        button = QPushButton(self)
-        button.move(100,70)
-        button.clicked.connect(self.on_off)
-
     @pyqtSlot()
     def on_off(self):
         if GPIO.input(LED):
             GPIO.output(LED,GPIO.LOW)
-            self.button.setText('OFF')
-
+            self.btn.setText('OFF')
+            self.status.setText('LED : OFF')
         else:
-            GPIO.input(LED)
             GPIO.output(LED,GPIO.HIGH)
             self.button.setText('ON')
-
+            self.status.setText('LED : ON')
         #self.close()
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    ex.show()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+widget = Win()
+widget.show()
+sys.exit(app.exec_())
