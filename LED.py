70794eecbe1655f8c4124bdc7262401d0fb698d9
paragj01 <paragjibhakate01@gmail.com>
2022-02-09 21:27:31 +0530

    LED Correction

--- LED.py
+++ LED.py
@@ -3,12 +3,12 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QDialog
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.uic import loadUi
 from matplotlib.widgets import Widget
-import RPi.GPIO as GPIO
+""" import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 LED = 26
-GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(LED, GPIO.OUT) """
 
 class Win(QDialog):
 
@@ -20,14 +20,14 @@ class Win(QDialog):
     
     @pyqtSlot()
     def on_off(self):
-        if GPIO.input(LED):
+        """ if GPIO.input(LED):
             GPIO.output(LED,GPIO.LOW)
             self.btn.setText('OFF')
             self.status.setText('LED : OFF')
         else:
             GPIO.output(LED,GPIO.HIGH)
             self.button.setText('ON')
-            self.status.setText('LED : ON')
+            self.status.setText('LED : ON') """
         #self.close()
 app = QApplication(sys.argv)
 widget = Win()
